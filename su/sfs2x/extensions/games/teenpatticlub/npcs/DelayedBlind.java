15bfafc3d9b96f7873ec215e0918647eb0abbcc6
Tz3entch <grayjoy1@gmail.com>
2015-11-13 11:10:08 +0300

    performance issue fixed, but there is still gamelogic 295 exeption

---
+++ su/sfs2x/extensions/games/teenpatticlub/npcs/DelayedBlind.java
@@ -0,0 +1,24 @@
+package su.sfs2x.extensions.games.teenpatticlub.npcs;
+
+import com.smartfoxserver.v2.entities.data.ISFSObject;
+import su.sfs2x.extensions.games.teenpatticlub.bean.GameBean;
+import su.sfs2x.extensions.games.teenpatticlub.bsn.ActionsBsn;
+
+/**
+ * Created by Сережа on 13.11.2015.
+ */
+public class DelayedBlind implements Runnable {
+    String player;
+    ISFSObject sfso;
+    GameBean gameBean;
+    ActionsBsn absn = new ActionsBsn();
+    public DelayedBlind (String player, ISFSObject sfso, GameBean gameBean) {
+        this.player=player;
+        this.sfso=sfso;
+        this.gameBean=gameBean;
+    }
+    @Override
+    public void run() {
+        absn.blind(player,sfso,gameBean);
+    }
+}
